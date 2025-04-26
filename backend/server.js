e7db490b70fb6ae8f5464aebb685d6c94a34f047
gsk-007 <gsingh16742@gmail.com>
2025-04-26 06:40:17 +0530

    feat: add cors and utility scripts

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-errors";
+import cors from "cors";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import { feedbackRoutes } from "./routes/feedbackRoutes.js";
 
@@ -7,6 +8,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 app.get("/health-check", (req, res) => {
   res.send("API is running");
